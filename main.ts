b47d63070e161b0340df017980b0afd20c5b39e7
Wagner <wprodriguesbr@gmail.com>
2023-05-24 17:16:03 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+let motor_ligado = 0
+input.onButtonPressed(Button.A, function () {
+    motor_ligado = 1
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
